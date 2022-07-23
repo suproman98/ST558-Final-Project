086b0cea44fecc12b97962e1de89274ee59c2ef0
Suproman98 <suprotik1@gmail.com>
2022-07-23 15:52:15 -0500

    Commit 3: Updated "Data" Page with download button and one subsettable option. Cleaned up theme and a few other dataset variables.

--- ShinyMon/server.R
+++ ShinyMon/server.R
@@ -15,27 +15,66 @@ library(shiny)
 library(DT)
 library(stringr)
 pokemon <- read_csv("pokemon.csv")
+pokemon <- pokemon %>% mutate(catch_rate_pct = round((catch_rate/255)*100)) %>% select(-catch_rate)
 
 function(input, output, session) {
     
     output$title <- renderUI({
         text <- paste("Relationship between ", input$attr1, " and ", input$attr2, " from Generation ", input$gen, " Pokemon", sep = "")
-        str_to_title(text)
+        h3(text)
         tools::toTitleCase(text)
     })
     
     
-    output$plot <- renderPlot({
+    output$corrplot <- renderPlot({
         pokemon %>%
             filter(generation == input$gen) %>%
-            select(attr1 = input$attr1, attr2 = input$attr2, len = input$attr1, name = name) %>%
+            select(attr1 = input$attr1, attr2 = input$attr2, name = name, type = type_1) %>%
             ggplot(aes(attr1, attr2))+
-            geom_point(aes(color=len, text=name), size = 2, alpha = 0.7)+
+            geom_point(aes(text=name, color=type), size = 2, alpha = 0.7)+
             geom_smooth(method = "lm", size=1.5)+
-            theme_light()+
             labs(x=paste(input$attr1), y=paste(input$attr2))
     })
     
-    output$table <- renderDataTable({pokemon})
+    output$catsum <- renderDataTable({
+        pokemon %>%
+            filter(type_1 == input$type1) %>%
+            filter(generation == input$gen) %>%
+            select(name = name, total = total_points, catchpct = catch_rate)
+    })
     
+    
+    output$numsum <- renderDataTable({
+        if(input$type1){
+            if(input$type2){
+                pokemon %>%
+                    select(name, generation, type_1, type_2, hp, attack, defense, sp_attack, sp_defense, speed, total_points)
+                    filter(type_1 == input$type1, type_2 == input$type2) %>%
+                    summarise(mean = avg(total_points), median = median(total_points), sd = sd(total_points))
+            } else {
+                pokemon %>%
+                    filter(type_1 == input$type1) %>%
+                    summarise(mean = avg(total_points), median = median(total_points), sd = sd(total_points))
+            }
+        } else {
+            print("Select a valid typing")
+        }
+    })
+    
+    
+    
+    
+        ### Subset data
+        pokesub <- reactive({ 
+            a <- subset(pokemon, type_1 %in% input$type)
+        return(a)
+        })
+    
+        output$subset <- renderDataTable({pokesub()})
+        output$download <- downloadHandler(
+            filename = function(){"PokemonData.csv"}, 
+            content = function(fname){
+                write.csv(dataset(), fname)
+            }
+        )
 }
--- ShinyMon/ui.R
+++ ShinyMon/ui.R
@@ -15,36 +15,58 @@ library(dplyr)
 library(shiny)
 library(DT)
 library(stringr)
+library(shinythemes)
 
 pokemon <- read_csv("pokemon.csv")
+pokemon <- pokemon %>% mutate(catch_rate_pct = round((catch_rate/255)*100)) %>% select(-catch_rate)
 
-navbarPage(
-    "My Application",
-    tabPanel("About This App", 
-             mainPanel(
-                 h3("Purpose"),
-                 br(),
-                 p("The purpose of this app is to explore the world of pokemon and understand diffeerent relationships between typing, catch rate, and overall power. Is there a clear-cut pokemon type that is strong or hard to catch throughout each generation? Which typing stands out the most in terms of total stats? Is there a correlation between weight and specific typing? Our data exploration (and yours) through this app will help us to better answer these questions."),
-                 br(),
-                 h3("Data Source"),
-                 p("This data consists of about 900 different pokemon across 8 generations. 47 different variables are being taken into account, such as indivdual stats (such as hitpoints or attack), typing (such as poison or fairy), damage multiplier (whether a move is super-effective or weak), and other aspects that influence a pokemon. This dataset was taken from ",
-                   tags$a(href = "https://www.kaggle.com/mariotormo/complete-pokemon-dataset-updated-090420", "Kaggle"),
-                   "which in turn scraped the data from Serebii, an official community website for pokemon."),
-                 br(),
-                 h3("Pages"),
-                 p("Each page corresponds to a dynamic interface, allowing anyone to create plots or toggle information with certain filters/buttons. The", strong("Data Exploration"), "tab will allow you to create various summaries for pokemon based on the variables used. Our focus will be TotalStats, a variable that has been created to aggregate all of the individual stats of a pokemon. The", strong("Modeling Page"), "will show some Machine Learning models that will give an idea of how well we can fit our models to new pokemon data (as there are more games that will be released. There will be individual tabs on this page that will give benefits/drawbacks to each model, some toggle settings to let the user control what goes into these models, and give the user options to play around with predictor values to gain predictions based on the responses. Lastly, there will be a", strong("Data"), "page that will allow the user to scroll through the data set and subset/export the data for their own use."),
-                 br(),
-                 h3("Gotta Catch Em All!"),
-                 tags$img(src="pokemon.jpg")
-             )),
-    tabPanel("Data Exploration",
-             sidebarLayout(
-                 sidebarPanel(
-                   selectInput('attr1', 'Inform first attribute (X-axis)', choices = c("hp", "speed", "attack", "defense", "sp_attack", "sp_defense")),
-                   selectInput('attr2', 'Inform second attribute (Y-axis)', choices = c("hp", "speed", "attack", "defense", "sp_attack", "sp_defense"), selected = "attack"),
-                   sliderInput('gen', 'Starting from generation:', min = 1, max = 8, value = 1, step = 1)
-                   ),
-                 mainPanel(strong(uiOutput("title", align = "center")), plotOutput("plot"))
-             )),
-    tabPanel("Modeling", mainPanel(dataTableOutput("table")))
+navbarPage(theme = shinytheme("sandstone"),
+  "My Application",
+  tabPanel("About This App", 
+           mainPanel(
+             h3("Purpose"),
+             br(),
+             p("The purpose of this app is to explore the world of pokemon and understand diffeerent relationships between typing, catch rate, and overall power. Is there a clear-cut pokemon type that is strong or hard to catch throughout each generation? Which typing stands out the most in terms of total stats? Is there a correlation between weight and specific typing? Our data exploration (and yours) through this app will help us to better answer these questions."),
+             br(),
+             h3("Data Source"),
+             p("This data consists of about 900 different pokemon across 8 generations. 47 different variables are being taken into account, such as indivdual stats (such as hitpoints or attack), typing (such as poison or fairy), damage multiplier (whether a move is super-effective or weak), and other aspects that influence a pokemon. This dataset was taken from ",
+               tags$a(href = "https://www.kaggle.com/mariotormo/complete-pokemon-dataset-updated-090420", "Kaggle"),
+               "which in turn scraped the data from Serebii, an official community website for pokemon."),
+             br(),
+             h3("Pages"),
+             p("Each page corresponds to a dynamic interface, allowing anyone to create plots or toggle information with certain filters/buttons. The", strong("Data Exploration"), "tab will allow you to create various summaries for pokemon based on the variables used. Our focus will be TotalStats, a variable that has been created to aggregate all of the individual stats of a pokemon. The", strong("Modeling Page"), "will show some Machine Learning models that will give an idea of how well we can fit our models to new pokemon data (as there are more games that will be released. There will be individual tabs on this page that will give benefits/drawbacks to each model, some toggle settings to let the user control what goes into these models, and give the user options to play around with predictor values to gain predictions based on the responses. Lastly, there will be a", strong("Data"), "page that will allow the user to scroll through the data set and subset/export the data for their own use."),
+             br(),
+             h3("Gotta Catch Em All!"),
+             tags$img(src="pokemon.jpg")
+           )),
+  
+  
+  
+  tabPanel("Data Exploration",
+           sidebarLayout(
+             sidebarPanel(
+               selectInput('attr1', 'Inform first attribute (X-axis)', choices = c("hp", "attack", "defense", "sp_attack", "sp_defense", "speed")),
+               selectInput('attr2', 'Inform second attribute (Y-axis)', choices = c("hp", "attack", "defense", "sp_attack", "sp_defense", "speed"), selected = "attack"),
+               sliderInput('gen', 'Select Generation', min = 1, max = 8, value = 1, step = 1),
+               br(),
+               h4("Generate Summary Statistics for Type"),
+               selectInput('type1', 'Select Typing', choices = c("Bug", "Dark", "Dragon", "Electric", "Fairy", "Fighting", "Fire", "Flying", "Ghost", "Grass", "Ground", "Ice", "Normal", "Poison", "Psychic", "Rock", "Steel", "Water"))
+             ),
+             mainPanel(strong(uiOutput("title", align = "center")), plotOutput("corrplot"), dataTableOutput("catsum"))
+           )),
+  
+  
+  tabPanel("Modeling", mainPanel(tabsetPanel(tabPanel("Model Info"),
+                                             tabPanel("Model Fitting"),
+                                             tabPanel("Prediction")))
+  ),
+  
+  
+  tabPanel("Data",
+           sidebarLayout(
+             sidebarPanel(
+               selectInput('type', 'Select Typing', choices = c("Bug", "Dark", "Dragon", "Electric", "Fairy", "Fighting", "Fire", "Flying", "Ghost", "Grass", "Ground", "Ice", "Normal", "Poison", "Psychic", "Rock", "Steel", "Water"), multiple = TRUE, selectize = TRUE, selected = c("Bug", "Dark", "Dragon", "Electric", "Fairy", "Fighting", "Fire", "Flying", "Ghost", "Grass", "Ground", "Ice", "Normal", "Poison", "Psychic", "Rock", "Steel", "Water")),
+               downloadButton('download',"Download the data")
+             ), mainPanel(dataTableOutput("subset"))
+           ))
 )
