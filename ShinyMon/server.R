a1847b110d42d61ecc28ffff4fd62e49b524092a
Suproman98 <suprotik1@gmail.com>
2022-07-22 19:14:57 -0500

    Commit 2: Updated Data and Exploration by adding correlation plot (need to adjust) and finished About section

--- ShinyMon/server.R
+++ ShinyMon/server.R
@@ -10,14 +10,30 @@
 library(dplyr)
 library(ggplot2)
 library(tidyr)
+library(dplyr)
 library(shiny)
 library(DT)
-pokemon <- read_csv("~/pokemon.csv")
+library(stringr)
+pokemon <- read_csv("pokemon.csv")
 
-function(input, output) {
+function(input, output, session) {
     
-    output$text <- renderText({
-        
+    output$title <- renderUI({
+        text <- paste("Relationship between ", input$attr1, " and ", input$attr2, " from Generation ", input$gen, " Pokemon", sep = "")
+        str_to_title(text)
+        tools::toTitleCase(text)
+    })
+    
+    
+    output$plot <- renderPlot({
+        pokemon %>%
+            filter(generation == input$gen) %>%
+            select(attr1 = input$attr1, attr2 = input$attr2, len = input$attr1, name = name) %>%
+            ggplot(aes(attr1, attr2))+
+            geom_point(aes(color=len, text=name), size = 2, alpha = 0.7)+
+            geom_smooth(method = "lm", size=1.5)+
+            theme_light()+
+            labs(x=paste(input$attr1), y=paste(input$attr2))
     })
     
     output$table <- renderDataTable({pokemon})
